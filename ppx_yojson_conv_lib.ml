bcc43bec7b37c4770348082d207fe208f7b22d0d
Xavier Clerc <xclerc@janestreet.com>
2020-03-31 09:48:49 +0100

    v0.14-preview.122.15+11

--- ppx_yojson_conv_lib.ml
+++ ppx_yojson_conv_lib.ml
@@ -3,6 +3,12 @@ module Yojson_conv = Yojson_conv
 module Yojsonable = Yojsonable_intf
 module Yojson = Yojson
 
+module Option = struct
+  type 'a t = 'a option =
+    | None
+    | Some of 'a
+end
+
 external ignore : _ -> unit = "%ignore"
 external poly_equal : 'a -> 'a -> bool = "%equal"
 
